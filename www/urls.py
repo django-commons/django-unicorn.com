5271f87d59a453bfefd3a6c8ef30d9336cbf83d7
adamghill <adamghill@yahoo.com>
2026-02-16 14:13:44 -0500

    Format code.

--- www/urls.py
+++ www/urls.py
@@ -10,15 +10,25 @@ urlpatterns = [
     path("articles", views.articles, name="articles"),
     path("sponsors", views.sponsors, name="sponsors"),
     # screencasts
-    re_path(r"^screencasts/(?P<name>[\w/-]+)$", views.screencasts, name="screencasts",),
+    re_path(
+        r"^screencasts/(?P<name>[\w/-]+)$",
+        views.screencasts,
+        name="screencasts",
+    ),
     path("screencasts", RedirectView.as_view(url="/screencasts/installation")),
     # examples
-    re_path(r"^examples/(?P<name>[\w/-]+)$", views.examples, name="examples",),
+    re_path(
+        r"^examples/(?P<name>[\w/-]+)$",
+        views.examples,
+        name="examples",
+    ),
     path("examples", RedirectView.as_view(url="/examples/todo")),
     # old documentation path
     path("documentation", views.documentation, name="documentation"),
     re_path(
-        r"^documentation/(?P<name>[\w/-]+)$", views.documentation, name="documentation",
+        r"^documentation/(?P<name>[\w/-]+)$",
+        views.documentation,
+        name="documentation",
     ),
     # sphinx-generated documentation
     re_path(r"docs/(?P<name>[\w-]+[^/])$", views.docs_redirect),
